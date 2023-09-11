ae7a373bac8f17f787a7ff9f6d4c4587c8dda42c
Moses Arulvin <41280678+mosesarulvin@users.noreply.github.com>
2023-09-11 22:34:14 +0530

    Changes for routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 
 function App() {
+  let base = "/portfolio/"
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/portfolio/" element={<Home/>} />
+        <Route path="/portfolio/about" element={<About/>} />
+        <Route path="/portfolio/contact" element={<Contact/>} />
+        <Route path="/portfolio/projects" element={<Portfolio/>} />
       </Routes>
     </BrowserRouter>
   )
